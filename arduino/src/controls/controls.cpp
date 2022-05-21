b11d23657a701a2ddd3dadec72f40f1662a5c329
Netz <bozo2000d@gmail.com>
2022-05-21 16:12:13 +0200

    Controls refactoring

--- arduino/src/controls/controls.cpp
+++ arduino/src/controls/controls.cpp
@@ -1,15 +1,9 @@
 #include "controls.h"
 
-CarPostion position;
+pos position = NULL;
 
 void control_init()
 {
-
-#ifdef DEBUG
-    Serial.printf("### Controls - init \n");
-#endif
-
-    delay(500);
 
     // Updating PWM frequency
 
@@ -28,34 +22,40 @@ void control_init()
     analogWrite(PWM_B, 0);
     analogWrite(PWM_L, 0);
     analogWrite(PWM_R, 0);
+
+    position = (pos)malloc(sizeof(CarPosition));
+
+#ifdef DEBUG
+    Serial.printf("### Controls - init \n");
+    if (position == NULL)
+        Serial.printf("Failed to allocate memory.\n");
+#endif
 }
 
 void control_update(char *in_buffer)
 {
 
-#ifdef DEBUG
-    Serial.printf("### Controls - update pins \n");
-#endif
-
     // Intepreting buffer position into local variables
 
-    sscanf(in_buffer, "%u&%u:%u&%ue", &(position.forward), &(position.backward), &(position.left), &(position.right));
+    sscanf(in_buffer, "%u&%u:%u&%ue", &(position->forward), &(position->backward), &(position->left), &(position->right));
+
+#ifdef DEBUG
+    Serial.printf("### Controls - update pins \nforward: %u backward: %u left: %u right: %u \n", position->forward, position->backward, position->left, position->right);
+#endif
 
     // Updating GPIO pins state by local variables values
 
-    if (position.forward != position.backward) // if not 0 both
+    if (position->forward != position->backward) // if not 0 both
     {
-        if (position.backward == 0)
+        if (position->backward == 0)
         {
             analogWrite(PWM_B, 0);
-            delayMicroseconds(200);
-            analogWrite(PWM_F, position.forward);
+            analogWrite(PWM_F, position->forward);
         }
-        if (position.forward == 0)
+        if (position->forward == 0)
         {
             analogWrite(PWM_F, 0);
-            delayMicroseconds(200);
-            analogWrite(PWM_B, position.backward);
+            analogWrite(PWM_B, position->backward);
         }
     }
     else
@@ -64,19 +64,17 @@ void control_update(char *in_buffer)
         analogWrite(PWM_B, 0);
     }
 
-    if (position.left != position.right) // if not 0 both
+    if (position->left != position->right) // if not 0 both
     {
-        if (position.right == 0)
+        if (position->right == 0)
         {
             analogWrite(PWM_R, 0);
-            delayMicroseconds(200);
-            analogWrite(PWM_L, position.left);
+            analogWrite(PWM_L, position->left);
         }
-        if (position.left == 0)
+        if (position->left == 0)
         {
             analogWrite(PWM_L, 0);
-            delayMicroseconds(200);
-            analogWrite(PWM_R, position.right);
+            analogWrite(PWM_R, position->right);
         }
     }
     else
@@ -85,5 +83,4 @@ void control_update(char *in_buffer)
         analogWrite(PWM_R, 0);
     }
 
-    // in_buffer[0] = '\0';
 }
--- arduino/src/controls/controls.h
+++ arduino/src/controls/controls.h
@@ -12,19 +12,16 @@
 
 #define DEBUG // DEBUG = 0 -> DEBUG mode ON, otherwise OFF
 
-
 #include <ESP8266WiFi.h>
 
-
-
-#define new_frequency 50 // use PWM frequency optimised for your hardware
+#define new_frequency 10 // use PWM frequency optimised for your hardware
 
 // Pinout definition
 
-#define PWM_F 5  // forward, D2
-#define PWM_B 16 // backward, D1
-#define PWM_L 0  // left, D3
-#define PWM_R 4  // right, D4
+#define PWM_F 2  // forward, D2
+#define PWM_B 15 // backward, D1
+#define PWM_L 4  // left, D3
+#define PWM_R 0  // right, D4
 
 // structure used for controling GPIO states
 
@@ -36,14 +33,12 @@ typedef struct _pos
   size_t backward;
   size_t left;
   size_t right;
-} CarPostion;
-
+} CarPosition;
 
 // Initialising GPIO outputs
 void control_init();
 
 // Intepreting in_buffer into GPIO output pins state
 void control_update(char *in_buffer);
-
 
 #endif
