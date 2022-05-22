77e83efc4c2597be77e09668aa33817560ff8940
Netz <bozo2000d@gmail.com>
2022-05-22 21:55:20 +0200

    Fixed up voltage calculator equation

--- arduino/src/monitoring/batteryMetric.cpp
+++ arduino/src/monitoring/batteryMetric.cpp
@@ -30,8 +30,26 @@ inline float readVoltage(int pin)
     int val;
     float Vin;
 
-    val = analogRead(pin);        // reads the analog input
-    Vin = (val * 5.00) / 2048.00; // formula for calculating voltage out
+    val = analogRead(pin); // reads the analog input
+
+    /**
+     *
+     * ESP reads voltage 0 - 1 V
+     * Croduino Nova2 has voltage divider and increases range up to 0 - 5 V
+     * Another voltage divider is added to increase voltage up to 0 - 10 V
+     * 1024 = 1 V
+     *
+     * U1 - Voltage at ESP8266 after voltage dividers
+     * U3 - Input voltage to voltage dividers
+     *
+     * U1 = 1/5 U2
+     * U2 = 1/2 U3
+     *-------------
+     * U3 = 10 U1
+     *
+     */
+
+    Vin = (val * 10.00) / 1024.00; // formula for calculating voltage out
 
     return Vin;
 }
